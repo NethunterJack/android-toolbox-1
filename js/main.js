432c56d09d8a0ef76db2a7fe70ac8a3bd7648beb
Andrzej Tok <atoktoto@gmail.com>
2014-01-24 21:05:22 +0100

    Transition to handlebars.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-
-
 $(function() {
 
 	$.ajax({
-		url: "parts/top-menu-items.html",
+		url: "parts/top-menu.html",
 		success: function (data) { $('#top-menu').append(makePretty(data)); },
 		dataType: 'html'
 	});
 
+	
 	$.ajax({
-		url: "parts/main-content.html",
-		success: function (data) { $('#main-content').append(makePretty(data)); },
+		url: "parts/link-list-template.html",
+		success: function (data) { 
+			var source = data;
+			var template = Handlebars.compile(source);
+			renderLinks(template);
+		},
 		dataType: 'html'
 	});
+	
+	Handlebars.registerHelper('link', function(url, text) {
+	  text = Handlebars.Utils.escapeExpression(text);
+	  url  = Handlebars.Utils.escapeExpression(url);
+
+	  var result = '<a href="' + url + '">' + text + '</a>';
+
+	  return new Handlebars.SafeString(result);
+	});
+	
+	
+	function renderLinks(template) {
+		$.ajax({
+			url: "content/links.json",
+			success: function (data) { 
+				var context = data;
+				var html = template(context);
+				$('#main-content').append(html); 
+			},
+			dataType: 'json'
+		});
+	}
 	
 	function makePretty(html) {
 		var tree = $("<div>" + html + "</div>");
