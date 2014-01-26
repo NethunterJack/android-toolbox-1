3ed62f051adda2449bf286185bb7aa23cf0c7066
Andrzej Tok <atoktoto@gmail.com>
2014-01-26 10:46:22 +0100

    Added contribute page

--- js/main.js
+++ js/main.js
@@ -1,31 +1,80 @@
 $(function() {
-
-	$.ajax({
-		url: "parts/top-menu.html",
-		success: function (data) { $('#top-menu').append(data); },
-		dataType: 'html'
-	});
-
 	
 	$.ajax({
-		url: "parts/link-list-template.html",
+		url: "parts/top-menu.html",
 		success: function (data) { 
-			var source = data;
-			var template = Handlebars.compile(source);
-			renderLinks(template);
+			$('#top-menu').append(data); 
+			menuPrepare();
 		},
 		dataType: 'html'
 	});
+
+	showLinks();
 	
 	Handlebars.registerHelper('link', function(url, text) {
 	  text = Handlebars.Utils.escapeExpression(text);
 	  url  = Handlebars.Utils.escapeExpression(url);
-
 	  var result = '<a href="' + url + '">' + text + '</a>';
-
 	  return new Handlebars.SafeString(result);
 	});
 	
+	Handlebars.registerHelper('gravatar', function(hash) {
+	  var size = 80;
+	  var url = 'http://www.gravatar.com/avatar/' + hash + '.jpg?s=' + size;
+	  var result = '<img src="' + url + '"/>';
+	  return new Handlebars.SafeString(result);
+	});
+	
+	function showLinks() {
+		$.ajax({
+			url: "parts/link-list-template.html",
+			success: function (data) { 
+				var source = data;
+				var template = Handlebars.compile(source);
+				$('#main-content').empty();
+				renderLinks(template);
+			},
+			dataType: 'html'
+		});
+	}
+	
+	function showContribute() {
+		$.ajax({
+			url: "parts/contribute.html",
+			success: function (data) { 
+				
+				var source = data;
+				var template = Handlebars.compile(source);
+				$('#main-content').empty();
+				renderContributors(template);
+			},
+			dataType: 'html'
+		});
+	}
+	
+	function menuPrepare() {
+		$("#home_frame").click(function(event){
+			event.preventDefault();
+			showLinks();
+		});
+	
+		$("#contribute_frame").click(function(event){
+			event.preventDefault();
+			showContribute();
+		});
+	}
+	
+	function renderContributors(template) {
+		$.ajax({
+			url: "content/contributors.json",
+			success: function (data) { 
+				var context = data;
+				var html = template(context);
+				$('#main-content').append(html); 
+			},
+			dataType: 'json'
+		});
+	}
 	
 	function renderLinks(template) {
 		$.ajax({
